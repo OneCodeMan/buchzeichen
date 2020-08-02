97b5501e9f01893b15f836a911d6d04565437dc9
dave gumba <dave_gumba@yahoo.com>
2020-08-02 17:30:22 -0400

    view bookmarks styling is done :)

--- includes/header.php
+++ includes/header.php
@@ -35,8 +35,26 @@ if (isset($_SESSION['email'])) {
 </head>
 <body>
 
-    <a href="includes/handlers/logout.php">
-        <i class="fa fa-sign-out fa-lg"></i>
-    </a>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Buchzeichen</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            </ul>
+            <span class="navbar-text">
+                <?php echo "Signed in as " . $_SESSION['email']; ?>
+            </span>
+            <span class="navbar-text">
+                <a class="nav-link" href="includes/handlers/logout.php">
+                    Log Out
+                </a>
+            </span>
+        </div>
+    </nav>
 </body>
 </html>
--- index.php
+++ index.php
@@ -3,6 +3,7 @@ include("includes/header.php");
 include("includes/classes/User.php");
 include("includes/classes/Bookmark.php");
 
+$first_name = $user['first_name'];
 $user = $user['email'];
 if(isset($_POST['add_bookmark'])) {
     $bookmark = new Bookmark($con, $user);
@@ -10,64 +11,68 @@ if(isset($_POST['add_bookmark'])) {
     header("Location: index.php");
 }
 ?>
+    <div class="container">
 
-<html>
-<head>
-    <title>Buchzeichen</title>
-</head>
-<body>
-    <h1>Buchzeichen</h1>
-    <p>Logged in as <?php echo $user;?></p>
+        <div id="bookmarks">
+            <h2 class="your-bookmarks-title">Your Bookmarks</h2>
+            <div class="col-md-9 add-bookmark-form-div">
+                <form action="index.php" class="add_bookmark_form" method="POST">
+                    <input type="text" name="bookmark_name" placeholder="name" required>
+                    <input type="text" name="bookmark_description" id="bookmark_description" placeholder="description" required>
+                    <input type="text" name="bookmark_url" id="bookmark_url" placeholder="url" required>
+                    <input type="submit" class="btn btn-warning" name="add_bookmark" id="add_bookmark" value="Add Bookmark">
+                    <br>
+                </form>
+            </div>
+            <?php 
+                $bookmarks_query = mysqli_query($con, "SELECT * FROM bookmarks WHERE belongs_to='$user'");
+                $bookmarks_query_row_count = mysqli_num_rows($bookmarks_query);
+                $bookmarks_html = '<div class="bookmarks">';
 
-    <div id="bookmarks">
-        <h2>Your Bookmarks</h2>
-        <form action="index.php" class="add_bookmark_form" method="POST">
-            <input type="text" name="bookmark_name" placeholder="name" required>
-            <textarea name="bookmark_description" id="bookmark_description" placeholder="Enter a brief description of the URL"></textarea>
-            <input type="text" name="bookmark_url" id="bookmark_url" placeholder="url" required>
-            <input type="submit" name="add_bookmark" id="add_bookmark" value="Post">
-            <br>
-        </form>
-        <?php 
-            $bookmarks_query = mysqli_query($con, "SELECT * FROM bookmarks WHERE belongs_to='$user'");
-            $bookmarks_query_row_count = mysqli_num_rows($bookmarks_query);
-            $bookmarks_html = '';
+                // loop to render the bookmarks
+                if($bookmarks_query_row_count > 0) { // make sure user has bookmarks
+                    while($row = mysqli_fetch_array($bookmarks_query)) { 
+                        // data extraction
+                        $id = $row['id'];
+                        $name = $row['name'];
+                        $description = $row['description'];
+                        $url = $row['url'];
+                        $belongs_to = $row['belongs_to'];
 
-            // loop to render the bookmarks
-            if($bookmarks_query_row_count > 0) { // make sure user has bookmarks
-                while($row = mysqli_fetch_array($bookmarks_query)) { 
-                    // data extraction
-                    $id = $row['id'];
-                    $name = $row['name'];
-                    $description = $row['description'];
-                    $url = $row['url'];
-                    $belongs_to = $row['belongs_to'];
-
-                    // data rendering
-                    $bookmarks_html .= "
-                        <div class='bookmark-container'>
-                            <a href='includes/handlers/forms/edit_bookmark.php?bookmark_id=$id&belongs_to=$belongs_to'>Edit</a>
-                            <button class='delete_button btn-danger' id='bookmark-delete-$id'>X</button>
-                            <a href='$url' target='_blank' bookmark-url'>$name</a>
-                            <p class='bookmark-description'>$description</p>
-                        </div>
-                    ";
-                ?>
-                <script>
-                    $(document).ready(function() {
-                        $('#bookmark-delete-<?php echo $id; ?>').on('click', function() {
-                            $.post("includes/handlers/forms/delete_bookmark.php?bookmark_id=<?php echo $id; ?>", function() {
-                                location.reload();
+                        // data rendering
+                        $bookmarks_html .= "
+                            <div class='bookmark-container'>
+                                <a href='$url' target='_blank' bookmark-url' class='bookmark-name'>$name</a>
+                                <a href='includes/handlers/forms/edit_bookmark.php?bookmark_id=$id&belongs_to=$belongs_to'>
+                                    <button class='edit-button btn-info'>
+                                        Edit
+                                    </button>
+                                </a>
+                                <button class='delete_button btn-danger' id='bookmark-delete-$id'>Delete</button>
+                                <p class='bookmark-description'>$description</p>
+                            </div>
+                            <hr>
+                        ";
+                    ?>
+                    <script>
+                        $(document).ready(function() {
+                            $('#bookmark-delete-<?php echo $id; ?>').on('click', function() {
+                                $.post("includes/handlers/forms/delete_bookmark.php?bookmark_id=<?php echo $id; ?>", function() {
+                                    location.reload();
+                                });
                             });
-                        });
-                    });
-                </script>
-                <?php
-                }
-            } // end of if
 
-            echo $bookmarks_html;
-        ?>
+                            
+                        });
+                    </script>
+                    <?php
+                    }
+                } // end of if
+                $bookmarks_html .= "</div>";
+                echo $bookmarks_html;
+            ?>
+        </div>
     </div>
+    
 </body>
 </html>
