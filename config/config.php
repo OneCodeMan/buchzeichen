b19528d54c7b0ee26b7f26c52902f1202e82a0ff
dave gumba <dave_gumba@yahoo.com>
2020-08-27 17:30:32 -0400

    changed db name

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php 
 ob_start();
 session_start();
-$con = mysqli_connect("us-cdbr-east-02.cleardb.com", "bf37c144838c8c", "40fa23a7", "buchzeichen");
+$con = mysqli_connect("us-cdbr-east-02.cleardb.com", "bf37c144838c8c", "40fa23a7", "heroku_25c33d2a731040c");
 
 if(mysqli_connect_errno()) {
     echo "Failed to connect: " . mysqli_connect_errno();
